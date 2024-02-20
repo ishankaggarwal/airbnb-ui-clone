bf82565eb2c9da223fe84c8e6a939d6e1100b5c7
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-02-19 21:04:35 -0800

    fixed md styling

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -4,7 +4,7 @@ import ListingCard from "./ListingCard";
 export default async function Listings() {
   const listings = await getListings();
   return (
-    <div className="p-10 gap-4 grid sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="p-10 gap-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {listings.map((l: any) => (
         <ListingCard key={l.id} listing={l} />
       ))}
